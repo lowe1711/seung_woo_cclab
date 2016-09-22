81ffbe0731fba062abd22423ec6a85e3d8222120
lowe1711 <AshleeW@Ashlees-Air.wireless.newschool.edu>
2016-09-22 19:02:10 -0400

    week2 and week3

---
+++ JavaScript/week2/week2_script.js
@@ -0,0 +1,13 @@
+$( function() {
+    $( "#draggable" ).draggable({ scroll: true });
+    $( "#draggable2" ).draggable({ scroll: true, scrollSensitivity: 100 });
+    $( "#draggable3" ).draggable({ scroll: true, scrollSpeed: 100 });
+     $( "#draggable4" ).draggable({ scroll: true });
+    $( "#draggable5" ).draggable({ scroll: true, scrollSensitivity: 100 });
+    $( "#draggable6" ).draggable({ scroll: true, scrollSpeed: 100 });
+     $( "#draggabl7" ).draggable({ scroll: true });
+    $( "#draggable8" ).draggable({ scroll: true, scrollSensitivity: 100 });
+    $( "#draggable9" ).draggable({ scroll: true, scrollSpeed: 100 });
+
+
+  } );
