95787b7916e5d4ce96dc1c43044338f2dce5eb87
lowe1711 <AshleeW@Ashlees-MacBook-Air.local>
2016-11-30 00:05:01 -0500

    unity
    rolling a ball (avoiding the capsule) game

---
+++ unity/rolling ball/Assets/Playercontroller.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Playercontroller : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	public Text countText;
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate (){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.CompareTag ("Pick Up")) {
+
+			other.gameObject.SetActive (false);
+			count = count + 1; 
+			SetCountText ();
+
+		}
+
+		if (other.gameObject.CompareTag ("Cap")) {
+
+			other.gameObject.SetActive (false);
+			count = count - 3; 
+			SetCountText ();
+
+		}
+	}
+	void SetCountText(){
+
+		countText.text = "Count:" + count.ToString ();
+		if (count >= 10) {
+
+			countText.text = "You win!";
+		}
+	}
+}
